12132e961717162081e58879ccbca495536755d6
taoqf <tao_qiufeng@126.com>
2020-03-20 09:44:46 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-export default function fun() {
+import { Linking } from 'react-native';
+
+/**
+ * 打开微信
+ */
+export default function open_wx(): Promise<boolean> {
+	return Linking.openURL('weixin://');
 }
